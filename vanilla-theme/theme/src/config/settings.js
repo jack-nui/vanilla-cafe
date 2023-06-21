970b07d862092466397c77de6d91bd7f703c90cd
Jack Daun <jackdaun@mail.com>
2023-06-21 22:14:12 +1200

    first commit

---
+++ vanilla-theme/theme/src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Vanilla Cafe | Belmont`,
+	description: `A gorgeous cafe with amazing food, fantastic coffee and lovely staff. Based right in the heart of Belmont.`,
+	url: `https://vanillacafe.co.nz`, // No trailing slash!
+	name: `Vanilla Cafe`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: true,
+  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
